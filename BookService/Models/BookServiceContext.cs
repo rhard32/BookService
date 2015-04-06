1e294d6a242f4c2db284b2561b0bafbddc0c3676
rhard32 <rhard32@gmail.com>
2015-04-06 13:13:52 -0400

    Adding Books controller to model

--- BookService/Models/BookServiceContext.cs
+++ BookService/Models/BookServiceContext.cs
@@ -20,6 +20,8 @@ namespace BookService.Models
         }
 
         public System.Data.Entity.DbSet<BookService.Models.Author> Authors { get; set; }
+
+        public System.Data.Entity.DbSet<BookService.Models.Book> Books { get; set; }
     
     }
 }
