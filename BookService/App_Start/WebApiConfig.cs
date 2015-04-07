e353b20b5b2da4f3a61fbe68bad095801529c168
rhard32 <rhard32@gmail.com>
2015-04-07 15:11:12 -0400

    Added code to BookServiceContent in model and Formatter to  App Start
    Allows me to get JSON  tutorial was missing this step

--- BookService/App_Start/WebApiConfig.cs
+++ BookService/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace BookService
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
